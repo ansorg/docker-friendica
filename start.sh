2dcf13e51f18458681e880fdfd8c26a9171e647b
Enrico204 <enrico204@gmail.com>
2017-08-02 15:00:44 +0200

    Fixed sendmail configuration

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 echo "[www]" > /etc/php5/conf.d/sendmail.ini
 if [ "${SMTP_SSL}" == "y" ]; then
-    echo "sendmail_path = /usr/sbin/sendmail -H 'exec openssl s_client -quiet -tls1 -connect ${SMTP_HOST}:${SMTP_PORT}' -au${SMTP_USER} -ap${SMTP_PASS} -t -i" >> /etc/php5/conf.d/sendmail.ini
+    echo "sendmail_path = \"/usr/sbin/sendmail -H 'exec openssl s_client -quiet -tls1 -connect ${SMTP_HOST}:${SMTP_PORT}' -au${SMTP_USER} -ap${SMTP_PASS} -t -i\"" >> /etc/php5/conf.d/sendmail.ini
 else
-    echo "sendmail_path = /usr/sbin/sendmail -S ${SMTP_HOST}:${SMTP_PORT} -au${SMTP_USER} -ap${SMTP_PASS} -t -i" >> /etc/php5/conf.d/sendmail.ini
+    echo "sendmail_path = \"/usr/sbin/sendmail -S ${SMTP_HOST}:${SMTP_PORT} -au${SMTP_USER} -ap${SMTP_PASS} -t -i\"" >> /etc/php5/conf.d/sendmail.ini
 fi
 
 sed -i "s/localhost/${SERVER_NAME}/g" /etc/nginx/conf.d/default.conf
